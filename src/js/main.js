85fd3f6bbb05ed587019764afa982ce27a520fa8
retwedt <retwedt@gmail.com>
2016-05-10 16:01:46 -0500

    move demo code to an external file

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,18 @@
 // main.js
 
-console.log("this is main.js!");
+window.onload = function() {
 
+	console.log("this is main.js!");
+
+	var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create });
+
+	function preload () {
+		game.load.image('logo', 'img/phaser.png');
+	}
+
+	function create () {
+		var logo = game.add.sprite(game.world.centerX, game.world.centerY, 'logo');
+		logo.anchor.setTo(0.5, 0.5);
+	}
+
+};
