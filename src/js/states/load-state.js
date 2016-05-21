726642d5b76a603b682e50c61b6b39d3ca0ab2ca
Michael Hadley <michaelwesthadley@gmail.com>
2016-05-20 21:47:29 -0500

    LoadState - load screen

---
+++ src/js/states/load-state.js
@@ -0,0 +1,33 @@
+/**
+ * LoadState - this is the loading screen
+ */
+
+module.exports = LoadState;
+
+function LoadState(game) {}
+
+LoadState.prototype.preload = function () {    
+    // Images
+    this.load.atlasJSONHash("assets", "images/atlases/assets.png", 
+        "images/atlases/assets.json");
+
+    // Stand-in for a loading bar
+    this.loadingText = this.add.text(this.world.centerX, this.world.centerY, 
+        "0%", { 
+            font: "200px Arial", 
+            fill: "#000", 
+            align: "center" 
+        });
+    this.loadingText.anchor.set(0.5);
+};
+
+LoadState.prototype.loadRender = function () {
+    this.loadingText.setText(this.load.progress + "%");
+};
+
+LoadState.prototype.create = function () {
+    // Since load progress might not reach 100 in the load loop, manually do it
+    this.loadingText.setText("100%");
+
+    this.game.state.start("game");
+};
