5d095427c69c2d47ccd55cfcad3a16d24251ee41
retwedt <retwedt@gmail.com>
2016-05-22 11:36:59 -0500

    remove gameContainer variable
    Phaser.Game can accept a string reference to the id of the intended game
    object container

--- src/js/main.js
+++ src/js/main.js
@@ -2,8 +2,8 @@ var GameState = require("./states/game-state.js");
 var BootState = require("./states/boot-state.js");
 var LoadState = require("./states/load-state.js");
 
-var gameContainer = document.getElementById("game-container");
-var game = new Phaser.Game(800, 600, Phaser.AUTO, gameContainer);
+// var gameContainer = document.getElementById("game-container");
+var game = new Phaser.Game(800, 600, Phaser.AUTO, "game-container");
 game.state.add("boot", BootState);
 game.state.add("load", LoadState);
 game.state.add("game", GameState);
