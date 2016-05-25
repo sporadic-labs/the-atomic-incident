9691b4e95326cfc55bc98735bd8eaf404fd206fc
retwedt <retwedt@gmail.com>
2016-05-25 14:19:12 -0500

    add pickups and multiple wandering enemies

--- src/js/states/game-state.js
+++ src/js/states/game-state.js
@@ -7,6 +7,7 @@ module.exports = GameState;
 var Player = require("../game-objects/player.js");
 var Seeker = require("../game-objects/seeker-enemy.js");
 var Wander = require("../game-objects/wander-enemy.js");
+var Pickup = require("../game-objects/base-pickup.js");
 var Reticule = require("../game-objects/reticule.js");
 var ScoreKeeper = require("../helpers/score-keeper.js");
 
@@ -24,6 +25,7 @@ GameState.prototype.create = function () {
         foreground: this.game.add.group(this.world, "foreground")
     };
     this.enemies = this.game.add.group(this.groups.midground, "enemies");
+    this.pickups = this.game.add.group(this.groups.midground, "pickups");
 
     // Physics
     this.physics.startSystem(Phaser.Physics.ARCADE);
@@ -35,7 +37,7 @@ GameState.prototype.create = function () {
     this.reticule = new Reticule(this, this.groups.foreground);
 
     this.player = new Player(this.game, this.world.centerX, this.world.centerY,
-        this.groups.foreground, this.enemies, this.reticule);
+        this.groups.foreground, this.enemies, this.pickups, this.reticule);
     this.camera.follow(this.player);
     
     // Signal
@@ -51,9 +53,26 @@ GameState.prototype.create = function () {
             this.player, this._signal);
     }
 
-    // one wandering enemy
-    new Wander(this.game, 800, 800, this.enemies, this.player,
-        this._signal);
+    for (var i = 0; i < 48; i += 1) {
+        var pos;
+        do {
+            pos = new Phaser.Point(this.world.randomX, this.world.randomY);
+        } while (this.player.position.distance(pos) < 300);
+        new Wander(this.game, pos.x, pos.y, this.enemies, this.player,
+            this._signal);
+    }
+
+    // Random pickups
+    for (var i = 0; i < 32; i += 1) {
+        var pos;
+        var newType;
+        var t = this.game.rnd.integerInRange(0,1);
+        t === 1 ? newType = "gun" : newType = "laser";
+        do {
+            pos = new Phaser.Point(this.world.randomX, this.world.randomY);
+        } while (this.player.position.distance(pos) < 300);
+        new Pickup(this.game, pos.x, pos.y, this.pickups, newType);
+    }
 
     // Score
     this.scoreKeeper = new ScoreKeeper(this, this.groups.foreground,
