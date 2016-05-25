ef02059703e834d16396c931e341850a50381c07
retwedt <retwedt@gmail.com>
2016-05-24 20:57:49 -0500

    remove commented out lines

--- src/js/main.js
+++ src/js/main.js
@@ -2,7 +2,6 @@ var GameState = require("./states/game-state.js");
 var BootState = require("./states/boot-state.js");
 var LoadState = require("./states/load-state.js");
 
-// var gameContainer = document.getElementById("game-container");
 var game = new Phaser.Game(800, 600, Phaser.AUTO, "game-container");
 game.state.add("boot", BootState);
 game.state.add("load", LoadState);
