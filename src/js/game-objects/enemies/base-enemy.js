b01ea328626faa405714ff5b5a1d9d45c17b20ae
Michael Hadley <michaelwesthadley@gmail.com>
2016-05-26 09:33:31 -0500

    Trimming base enemy class to essentials

--- src/js/game-objects/enemies/base-enemy.js
+++ src/js/game-objects/enemies/base-enemy.js
@@ -1,36 +1,17 @@
 module.exports = BaseEnemy;
 
-var ANIM_NAMES = {
-    IDLE: "idle",
-    MOVE: "move"
-};
-
 // Prototype chain - inherits from Sprite
 BaseEnemy.prototype = Object.create(Phaser.Sprite.prototype);
 BaseEnemy.prototype.constructor = BaseEnemy;
 
-function BaseEnemy(game, x, y, parentGroup, target, signal) {
-    Phaser.Sprite.call(this, game, x, y, "assets", "enemy/idle-01");
+function BaseEnemy(game, x, y, parentGroup, target, scoreSignal) {
+    Phaser.Sprite.call(this, game, x, y);
     this.anchor.set(0.5);
     parentGroup.add(this);
 
-    this._signal = signal;
-    
-    // Give the sprite a random tint
-    var randLightness = this.game.rnd.realInRange(0.4, 0.6);
-    var rgb = Phaser.Color.HSLtoRGB(0.98, 1, randLightness);
-    this.tint = Phaser.Color.getColor(rgb.r, rgb.g, rgb.b);
-
-    // Setup animations
-    var idleFrames = Phaser.Animation.generateFrameNames("enemy/idle-", 1, 4, 
-        "", 2);
-    var moveFrames = Phaser.Animation.generateFrameNames("enemy/move-", 1, 4, 
-        "", 2);
-    this.animations.add(ANIM_NAMES.IDLE, idleFrames, 10, true);
-    this.animations.add(ANIM_NAMES.MOVE, moveFrames, 10, true);
-    this.animations.play(ANIM_NAMES.IDLE);
-
+    this._scoreSignal = scoreSignal;
     this._target = target;
+    this._pointsValue = 1;
 
     // Configure player physics
     this._maxSpeed = 200;
@@ -41,19 +22,6 @@ function BaseEnemy(game, x, y, parentGroup, target, signal) {
     this.body.setCircle(this.width / 2 * 0.8); // Fudge factor
 }
 
-/**
- * Override preUpdate to update velocity. Physics updates happen in preUpdate,
- * so if the velocity updates happened AFTER that, the targeting would be off
- * by a frame.
- */
-BaseEnemy.prototype.preUpdate = function () {
-    this.animations.play(ANIM_NAMES.IDLE);
-
-    // Call the parent's preUpdate and return the value. Something else in
-    // Phaser might use it...
-    return Phaser.Sprite.prototype.preUpdate.call(this);
-};
-
-BaseEnemy.prototype.deathCry = function () {
-    this._signal.dispatch();
+BaseEnemy.prototype.updateScore = function () {
+    this._scoreSignal.dispatch(this._pointsValue);
 };
