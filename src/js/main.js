7d15a40cc2b4c38cc5c89970062bf38bf3588001
retwedt <retwedt@gmail.com>
2016-05-28 15:18:58 -0500

    add start screen to game states

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,11 @@
 var GameState = require("./states/game-state.js");
 var BootState = require("./states/boot-state.js");
 var LoadState = require("./states/load-state.js");
+var StartScreen = require("./states/start-screen.js");
 
 var game = new Phaser.Game(800, 600, Phaser.AUTO, "game-container");
 game.state.add("boot", BootState);
 game.state.add("load", LoadState);
+game.state.add("start", StartScreen);
 game.state.add("game", GameState);
 game.state.start("boot");
