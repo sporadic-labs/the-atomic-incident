dfd9b8ae55cc6c28faa3c61a9369b1f5b3355587
retwedt <retwedt@gmail.com>
2016-05-28 15:28:15 -0500

    comment out some broken code in the start screen

--- src/js/states/start-screen.js
+++ src/js/states/start-screen.js
@@ -36,12 +36,12 @@ StartScreen.prototype.playTheGame = function () {
 };
 
 
-    create: function(){
-        var gameTitle = this.game.add.sprite(160,160,"gametitle");
-        gameTitle.anchor.setTo(0.5,0.5);
-        var playButton = this.game.add.button(160,320,"play",this.playTheGame,this);
-        playButton.anchor.setTo(0.5,0.5);
-    },
-    playTheGame: function(){
-        this.game.state.start("TheGame");
-    }
+    // create: function(){
+    //     var gameTitle = this.game.add.sprite(160,160,"gametitle");
+    //     gameTitle.anchor.setTo(0.5,0.5);
+    //     var playButton = this.game.add.button(160,320,"play",this.playTheGame,this);
+    //     playButton.anchor.setTo(0.5,0.5);
+    // },
+    // playTheGame: function(){
+    //     this.game.state.start("TheGame");
+    // }
