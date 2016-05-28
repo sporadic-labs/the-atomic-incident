416ebb53af281c4de3b6210e601d9870e6a856df
retwedt <retwedt@gmail.com>
2016-05-28 15:18:41 -0500

    rename guns folder to weapons

--- src/js/game-objects/weapons/laser.js
+++ src/js/game-objects/weapons/laser.js
@@ -27,10 +27,15 @@ Laser.prototype.fire = function (targetPos) {
         var angle = this._player.position.angle(targetPos); // Radians
         // Start bullet in a position along that trajectory, but in front of the
         // player
-        var bulletPos = this._player.position.clone();
-        bulletPos.x += (0.75 * this._player.width) * Math.cos(angle);
-        bulletPos.y += (0.75 * this._player.width) * Math.sin(angle);
-        new Bolt(this.game, bulletPos.x, bulletPos.y, this, angle, 
+        var bulletPosR = this._player.position.clone();
+        var bulletPosL = this._player.position.clone();
+        bulletPosR.x += (0.75 * this._player.width) * Math.cos(angle) + (10 * Math.sin(angle));
+        bulletPosR.y += (0.75 * this._player.width) * Math.sin(angle) + (10 * Math.cos(angle));
+        bulletPosL.x += (0.75 * this._player.width) * Math.cos(angle) - (10 * Math.sin(angle));
+        bulletPosL.y += (0.75 * this._player.width) * Math.sin(angle) - (10 * Math.cos(angle));
+        new Bolt(this.game, bulletPosR.x, bulletPosR.y, this, angle, 
+            this._enemies);
+        new Bolt(this.game, bulletPosL.x, bulletPosL.y, this, angle, 
             this._enemies);
         this._startCooldown();
     }
