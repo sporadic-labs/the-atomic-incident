3d6d40272fc6b06671cdcb8ff345fe2d942b8288
Michael Hadley <michaelwesthadley@gmail.com>
2016-05-29 16:30:53 -0500

    Update Player to take comboTracker parameter
    Player listens for combo updates and uses that to map to player speed

--- src/js/game-objects/player.js
+++ src/js/game-objects/player.js
@@ -16,7 +16,8 @@ var ANIM_NAMES = {
 Player.prototype = Object.create(Phaser.Sprite.prototype);
 Player.prototype.constructor = Player; // Make sure constructor reads properly
 
-function Player(game, x, y, parentGroup, enemies, pickups, reticule) {
+function Player(game, x, y, parentGroup, enemies, pickups, reticule, 
+    comboTracker) {
     // Call the sprite constructor, but instead of it creating a new object, it
     // modifies the current "this" object
     Phaser.Sprite.call(this, game, x, y, "assets", "player/idle-01");
@@ -26,6 +27,9 @@ function Player(game, x, y, parentGroup, enemies, pickups, reticule) {
     this._reticule = reticule;
     this._enemies = enemies;
     this._pickups = pickups;
+
+    this._comboTracker = comboTracker;
+    this._comboTracker.addListener(this._onComboUpdate, this);
 
     this._gunType = "gun";
     this._allGuns = {
@@ -52,7 +56,7 @@ function Player(game, x, y, parentGroup, enemies, pickups, reticule) {
     this.animations.play(ANIM_NAMES.IDLE);
 
     // Configure player physics
-    this._maxSpeed = 100;
+    this._maxSpeed = 50;
     this._maxAcceleration = 5000;
     this._maxDrag = 4000;
     game.physics.arcade.enable(this);
@@ -136,4 +140,10 @@ Player.prototype._onCollideWithPickup = function (self, pickup) {
         }
     }
     pickup.destroy();
+};
+
+Player.prototype._onComboUpdate = function (combo) {
+    var newSpeed = Phaser.Math.mapLinear(combo, 0, 50, 50, 500);
+    newSpeed = Phaser.Math.clamp(newSpeed, 50, 500);
+    this._maxSpeed = newSpeed; 
 };
