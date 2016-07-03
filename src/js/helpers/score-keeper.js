a97d35202a9bd561c95938a52da991046b199317
Michael Hadley <michaelwesthadley@gmail.com>
2016-07-02 22:53:29 -0500

    Score is globally available - no need for signals

--- src/js/helpers/score-keeper.js
+++ src/js/helpers/score-keeper.js
@@ -2,14 +2,15 @@ module.exports = ScoreKeeper;
 
 function ScoreKeeper(signal) {
 	this._score = 0;
-	this._signal = signal;
-
-	this._signal.add(this.updateScore, this);
 }
 
-ScoreKeeper.prototype.updateScore = function (points) {
+ScoreKeeper.prototype.incrementScore = function (points) {
     if (points === undefined) return;
     this._score += points;
+};
+
+ScoreKeeper.prototype.setScore = function (points) {
+    this._score = points || 0;
 };
 
 ScoreKeeper.prototype.getScore = function () {
