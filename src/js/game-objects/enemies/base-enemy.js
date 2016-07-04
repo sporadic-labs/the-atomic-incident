1c8c3e14994e71900743dc085f200b42aeac3ae7
Michael Hadley <michaelwesthadley@gmail.com>
2016-07-03 21:11:02 -0500

    BaseEnemy comes with a HealthBar
    BaseEnemy manages the positioning of the health bar and forwarding
    damage to the health bar

--- src/js/game-objects/enemies/base-enemy.js
+++ src/js/game-objects/enemies/base-enemy.js
@@ -1,6 +1,7 @@
 module.exports = BaseEnemy;
 
 var utils = require("../../helpers/utilities.js");
+var HealthBar = require("./health-bar.js");
 
 BaseEnemy.prototype = Object.create(Phaser.Sprite.prototype);
 
@@ -11,9 +12,13 @@ function BaseEnemy(game, x, y, key, frame, health, parentGroup, pointValue) {
 
     this._player = this.game.globals.player;
     this._scoreKeepter = this.game.globals.scoreKeeper;
-    this._health = health;
-    this._startingHealth = health;
     this._pointValue = utils.default(pointValue, 1);
+
+    // Health bar 
+    var cx = 0;
+    var cy = (this.height / 2) + 4;
+    this._healthBar = new HealthBar(game, this, parentGroup, cx, cy, 20, 4);
+    this._healthBar.initHealth(health);
 
     // Configure simple physics
     game.physics.arcade.enable(this);
@@ -22,11 +27,24 @@ function BaseEnemy(game, x, y, key, frame, health, parentGroup, pointValue) {
 }
 
 BaseEnemy.prototype.takeDamage = function (damage) {
-    this._health -= damage;
-    if (this._health <= 0) {
+    var newHealth = this._healthBar.incrementHealth(-damage);
+    if (newHealth <= 0) {
         this._scoreKeepter.incrementScore(this._pointValue);
         this.destroy();
         return true;
     }
     return false;
+};
+
+BaseEnemy.prototype.postUpdate = function () {
+    // Post updates are where movement physics are applied. We want all post
+    // updates to finish BEFORE placing extracting the sprite's position.
+    Phaser.Sprite.prototype.postUpdate.apply(this, arguments);
+    // Now extract sprite position and apply it to the group
+    this._healthBar.updatePosition();
+};
+
+BaseEnemy.prototype.destroy = function () {
+    this._healthBar.destroy();
+    Phaser.Sprite.prototype.destroy.apply(this, arguments);
 };
