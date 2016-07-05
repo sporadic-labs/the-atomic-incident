2fe4ddec9540a273565c582c8ae906cd1e743069
Michael Hadley <michaelwesthadley@gmail.com>
2016-07-04 21:19:54 -0500

    Add overlap checking util

--- src/js/helpers/sprite-utilities.js
+++ src/js/helpers/sprite-utilities.js
@@ -3,3 +3,15 @@ exports.applyRandomLightnessTint = function (sprite, h, s, l) {
     var rgb = Phaser.Color.HSLtoRGB(h, s, l);
     sprite.tint = Phaser.Color.getColor(rgb.r, rgb.g, rgb.b);
 };
+
+exports.checkOverlapWithGroup = function (self, group, callback, context) {
+    for (var i = 0; i < group.children.length; i += 1) {
+        var child = group.children[i];
+        if (child instanceof Phaser.Group) {
+            exports.checkOverlapWithGroup(self, child, callback, context);
+        } else {
+            self.game.physics.arcade.overlap(self, child, callback, null, 
+                context);
+        }
+    }
+};
