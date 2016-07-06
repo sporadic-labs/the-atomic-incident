165d3efbaaa55322cc42d842f372619d776edbfc
Rex Twedt <retwedt@gmail.com>
2016-07-05 21:00:06 -0500

    add default weapon using the test triangle asset to distinguish it

---
+++ src/js/game-objects/weapons/rock.js
@@ -0,0 +1,41 @@
+module.exports = Rock;
+
+var BaseWeapon = require("./base-weapon.js");
+var Projectile = require("./base-projectile.js");
+
+Rock.prototype = Object.create(BaseWeapon.prototype);
+
+// optional settings for projectiles
+var projectileOptions = {
+    isDestructible: true,
+    rotateOnSetup: true,
+    canBounce: false,
+    hiddenOnSetup: false
+};
+
+function Rock(game, parentGroup, player, cooldownTime, specialCooldownTime,
+    totalAmmo) {
+    BaseWeapon.call(this, game, parentGroup, "Rock", player, cooldownTime, 
+        specialCooldownTime, totalAmmo);
+}
+
+Rock.prototype.fire = function (targetPos) {
+    if (this.isAbleToAttack()) {
+        // Find trajectory
+        var angle = this._player.position.angle(targetPos); // Radians
+        // Start bullet in a position along that trajectory, but in front of 
+        // the player
+        var x = this._player.position.x + (0.75 * this._player.width) * 
+            Math.cos(angle);
+        var y = this._player.position.y + (0.75 * this._player.width) * 
+            Math.sin(angle);
+
+        this._createProjectile(x, y, angle);
+        this._startCooldown(this._cooldownTime);
+    }
+};
+
+Rock.prototype._createProjectile = function (x, y, angle) {
+    new Projectile(this.game, x, y, "assets", "test/bullet", this, 
+        this._player, 100, angle, 300, 500, projectileOptions);
+};
