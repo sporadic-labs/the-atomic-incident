94b90e7b88047cebcf38f708fae93d5935527203
Michael Hadley <michaelwesthadley@gmail.com>
2016-07-06 21:09:36 -0500

    Update checkOverlapGroup to run assuming everything has a SatBody

--- src/js/helpers/sprite-utilities.js
+++ src/js/helpers/sprite-utilities.js
@@ -4,39 +4,19 @@ exports.applyRandomLightnessTint = function (sprite, h, s, l) {
     sprite.tint = Phaser.Color.getColor(rgb.r, rgb.g, rgb.b);
 };
 
-exports.checkOverlapWithGroup = function (self, group, callback, context) {
+exports.checkOverlapWithGroup = function (sprite, group, callback, context) {
+    // Loop through children in group
     for (var i = 0; i < group.children.length; i += 1) {
         var child = group.children[i];
         if (child instanceof Phaser.Group) {
-            exports.checkOverlapWithGroup(self, child, callback, context);
+            // If child is a group, recursion time
+            exports.checkOverlapWithGroup(sprite, child, callback, context);
         } else {
-            self.game.physics.arcade.overlap(self, child, callback, null, 
-                context);
-        }
-    }
-};
-
-exports.satOverlapWithArcadeGroup = function (satPolygon, self, group, callback, 
-    context) {
-    for (var i = 0; i < group.children.length; i += 1) {
-        var child = group.children[i];
-        if (child instanceof Phaser.Group) {
-            exports.satOverlapWithArcadeGroup(satPolygon, self, child, callback, 
-                context);
-        } else {
-            // Check for a body
-            var body = child.body;
-            if (!body) continue;
-
-            // If there is a body, create a SAT polygon from the body. Assume 
-            // that the body's anchor is (0.5, 0.5) since all enemies are set up
-            // that way for now...
-            var w = body.width;
-            var h = body.height;
-            var pos = new SAT.Vector(body.x, body.y);  
-            var satOtherPolygon = new SAT.Box(pos, w, h).toPolygon();
-            var collided = SAT.testPolygonPolygon(satPolygon, satOtherPolygon);
-            if (collided) callback.call(context, self, child);
+            // If child is not a group, make sure it has a SAT body
+            if (!child.satBody) continue;
+            // Check overlap
+            var isOverlap = sprite.satBody.testOverlap(child.satBody);
+            if (isOverlap) callback.call(context, sprite, child);
         }
     }
 };
