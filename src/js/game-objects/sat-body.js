edb14fb17fb9e98cd5ba47e81f0e6107d32fbda9
Michael Hadley <michaelwesthadley@gmail.com>
2016-07-07 09:00:26 -0500

    Extend SatBody to handle Circle and Polygon shaped bodies

--- src/js/game-objects/sat-body.js
+++ src/js/game-objects/sat-body.js
@@ -6,23 +6,29 @@
  *   e.g. a box plus a circle?
  * - Do we need there to be a possible offset between the sprite's anchor and 
  *   this SatBody? If so, we need to track that.
+ * - Do we need to consider scale and pivot?
  */
 
 module.exports = SatBody;
 
-// Helper Factories
+var BODY_TYPE = {
+    CIRCLE: "circle",
+    POLYGON: "polygon"
+};
+
+// Helper Object Factories
 var Vec = function (x, y) {
     return new SAT.Vector(x, y);
 };
 var Box = function (pos, w, h) {
     return new SAT.Box(pos, w, h);
 };
-// var Circle = function (pos, r) {
-//     return new SAT.Circle(pos, r);
-// };
-// var Poly = function (pos, points) {
-//     return new SAT.Polygon(pos, points);
-// };
+var Circle = function (pos, r) {
+    return new SAT.Circle(pos, r);
+};
+var Polygon = function (pos, points) {
+    return new SAT.Polygon(pos, points);
+};
 
 function SatBody(sprite, isDebug) {
     this._sprite = sprite;
@@ -35,49 +41,63 @@ function SatBody(sprite, isDebug) {
 }
 
 SatBody.prototype.initBox = function (anchor) {
+    this._bodyType = BODY_TYPE.POLYGON;
     var s = this._sprite;
-    this._body = Box(Vec(s.x, s.y), s.width, s.height)
-        .toPolygon();
+    this._body = Box(Vec(s.x, s.y), s.width, s.height).toPolygon();
     this._body.translate(-anchor.x * s.width, -anchor.y * s.height);
 };
 
-// NOT IMPLEMENTED YET
-// SatBody.prototype.initCircle = function (x, y, r) {
-//     this._body = new SAT.Circle(new SAT.V(x, y), r);
-// };
-// SatBody.prototype.initPolygon = function (x, y, points) {
-//     this._body = new SAT.Polygon(new SAT.V(x, y), points);
-// };
+SatBody.prototype.initCircle = function (r) {
+    this._bodyType = BODY_TYPE.CIRCLE;
+    var s = this._sprite;
+    if (!r) r = s.width / 2;
+    this._body = Circle(Vec(s.x, s.y), r);
+};
+
+SatBody.prototype.initPolygon = function (points) {
+    // Untested
+    // This function would be more convient if it took an array or parsed the 
+    // arguments variable to construct the points
+    this._bodyType = BODY_TYPE.POLYGON;
+    var s = this._sprite;
+    this._body = Polygon(Vec(s.x, s.y), points);
+};
 
 SatBody.prototype.getBody = function () {
     return this._body;
 };
 
-SatBody.prototype.testOverlap = function (otherBody) {
-    return SAT.testPolygonPolygon(this._body, otherBody.getBody());   
+SatBody.prototype.getBodyType = function () {
+    return this._bodyType;
+};
 
-    // NOT IMPLEMENTED YET
-    // var isCircle = this._body instanceof SAT.Circle;
-    // var otherIsCircle = otherBody instanceof SAT.Circle;
-    // if (isCircle || otherIsCircle) {
-    //     if (isCircle && otherIsCircle) return SAT.testCircleCircle(this._body, otherBody);
-    //     else if (isCircle) return SAT.testCirclePolygon(this._body, otherBody);
-    //     else return SAT.testPolygonCircle(this._body, otherBody);
-    // } else {
-    //     return SAT.testPolygonPolygon(this._body, otherBody);   
-    // }
+SatBody.prototype.testOverlap = function (otherBody) {
+    // Handy boolean shorthands
+    var thisIsCircle = (this._bodyType === BODY_TYPE.CIRCLE);
+    var otherIsCircle = (otherBody._bodyType === BODY_TYPE.CIRCLE);
+
+    // Determine the appropriate collision body comparison
+    if (thisIsCircle && otherIsCircle) {
+        return SAT.testCircleCircle(this._body, otherBody._body);
+    } else if (!thisIsCircle && otherIsCircle) {
+        return SAT.testPolygonCircle(this._body, otherBody._body);
+    } else if (thisIsCircle && !otherIsCircle) {
+        return SAT.testPolygonCircle(otherBody._body, this._body);
+    } else {
+        return SAT.testPolygonPolygon(this._body, otherBody._body);
+    }
 };
 
 SatBody.prototype.update = function () {
-    if (this._body instanceof SAT.Circle) {
-        this._body.pos.x = this._sprite.x;
-        this._body.pos.y = this._sprite.y;
-    } else if (this._body instanceof SAT.Polygon) {
-        // Fix for now - position is based on world, but something similar 
-        // should probably be happening for the rotation
+    // Update the position of the colliding body
+    if (this._bodyType === BODY_TYPE.CIRCLE) {
+        this._body.pos.x = this._sprite.worldPosition.x;
+        this._body.pos.y = this._sprite.worldPosition.y;
+    } else if (this._bodyType === BODY_TYPE.POLYGON) {
         this._body.pos.x = this._sprite.worldPosition.x;
         this._body.pos.y = this._sprite.worldPosition.y;
         this._body.setAngle(this._sprite.rotation);
+        // Rotation should probably be world rotation...or something?
     }
 
     if (this._isDebug) this._updateDebug();
@@ -105,6 +125,11 @@ SatBody.prototype._updateDebug = function () {
     this._debugGraphics.clear();
     this._debugGraphics.lineStyle(1, this._debugColor, 0.6);
     this._debugGraphics.beginFill(this._debugColor, 0.4);
-    this._debugGraphics.drawPolygon(this._body.calcPoints);
+    if (this._bodyType === BODY_TYPE.CIRCLE) {
+        this._debugGraphics.drawCircle(this._body.x, this._body.y, 
+            2 * this._body.r);
+    } else if (this._bodyType === BODY_TYPE.POLYGON) {
+        this._debugGraphics.drawPolygon(this._body.calcPoints);
+    }
     this._debugGraphics.endFill();
 };
