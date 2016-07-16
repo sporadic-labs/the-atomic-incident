5fa2c4e4074798aed3a24802204784cf96079587
Rex Twedt <retwedt@gmail.com>
2016-07-16 18:12:37 -0500

    Remove obsolete 'game-state' import from main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,4 @@
 var Sandbox = require("./states/sandbox.js");
-var GameState = require("./states/game-state.js");
 var BootState = require("./states/boot-state.js");
 var LoadState = require("./states/load-state.js");
 var StartScreen = require("./states/start-screen.js");
@@ -8,6 +7,5 @@ var game = new Phaser.Game(800, 600, Phaser.AUTO, "game-container");
 game.state.add("boot", BootState);
 game.state.add("load", LoadState);
 game.state.add("start", StartScreen);
-game.state.add("game", GameState);
 game.state.add("sandbox", Sandbox);
 game.state.start("boot");
