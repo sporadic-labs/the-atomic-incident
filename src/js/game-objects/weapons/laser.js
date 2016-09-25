630748b7fd2862f4c0570a19428086f4bcdeafe9
Rex Twedt <retwedt@gmail.com>
2016-09-25 15:39:18 -0500

    Make some tweaks to the multi-shot laser gun.  Not really sure what to do with this, it isn't really as interesting or distinct as the rest of the weapons.  Find a way to improve this!

--- src/js/game-objects/weapons/laser.js
+++ src/js/game-objects/weapons/laser.js
@@ -15,20 +15,23 @@ var projectileOptions = {
 
 function Laser(game, parentGroup, player) {
     BaseWeapon.call(this, game, parentGroup, "Laser", player);
-    this.initAmmo(60);
-    this.initCooldown(200, 500);
+    this.initAmmo(120);
+    this.initCooldown(160, 500);
 }
 
 Laser.prototype.fire = function (targetPos) {
     if (this.isAbleToAttack() && !this.isAmmoEmpty()) {
         // Find trajectory
         var angle = this._player.position.angle(targetPos); // Radians
-        var spacing = 0.5 * this._player.width;
-        this._createProjectile(angle, 15, 0);
-        this._createProjectile(angle, 5, -spacing);
+        var spacing = 0.25 * this._player.width;
+        var spacing2 = 0.5 * this._player.width;
         this._createProjectile(angle, 5, spacing);
+        this._createProjectile(angle, 5, -spacing);
+        this._createProjectile(angle, 12, 0);
+        this._createProjectile(angle, -12, spacing2);
+        this._createProjectile(angle, -12, -spacing2);
 
-        this.incrementAmmo(-3);
+        this.incrementAmmo(-5);
 
         this._startCooldown(this._cooldownTime);
     }
@@ -42,7 +45,7 @@ Laser.prototype._createProjectile = function (angle, playerDistance,
     var y = this._player.y + (playerDistance * Math.sin(angle)) - 
         (perpendicularOffset * Math.sin(perpAngle));    
     var p = new Projectile(this.game, x, y, "assets", "weapons/laser-01", this,
-        this._player, 50, angle, 750, 500, projectileOptions);
+        this._player, 24, angle, 800, 500, projectileOptions);
     var rgb = Phaser.Color.HSLtoRGB(0.52, 0.5, 0.64);
     p.tint = Phaser.Color.getColor(rgb.r, rgb.g, rgb.b);
 };
