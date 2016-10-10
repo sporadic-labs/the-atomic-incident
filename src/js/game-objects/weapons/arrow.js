06b345d9626a675e8ff69c4d7c8ee15696045562
Rex Twedt <retwedt@gmail.com>
2016-10-09 22:12:30 -0500

    Initial commit of an arrow weapon.  Hopefully an initial pass at a bouncing weapon.

---
+++ src/js/game-objects/weapons/arrow.js
@@ -0,0 +1,45 @@
+module.exports = Arrow;
+
+var BaseWeapon = require("./base-weapon.js");
+var Projectile = require("./base-projectile.js");
+
+Arrow.prototype = Object.create(BaseWeapon.prototype);
+
+// optional settings for projectiles
+var projectileOptions = {
+    isDestructible: true,
+    rotateOnSetup: true,
+    canBounce: false,
+    hiddenOnSetup: false
+};
+
+function Arrow(game, parentGroup, player) {
+    BaseWeapon.call(this, game, parentGroup, "Arrow", player);
+    this.initAmmo(40);
+    this.initCooldown(360);
+}
+
+Arrow.prototype.fire = function (targetPos) {
+    if (this.isAbleToAttack() && !this.isAmmoEmpty()) {
+        // Find trajectory
+        var angle = this._player.position.angle(targetPos); // Radians
+        // Start bullet in a position along that trajectory, but in front of 
+        // the player
+        var x = this._player.position.x + (0.75 * this._player.width) * 
+            Math.cos(angle);
+        var y = this._player.position.y + (0.75 * this._player.width) * 
+            Math.sin(angle);
+
+        this.incrementAmmo(-1);
+
+
+        this._createProjectile(x, y, angle);
+        this._startCooldown(this._cooldownTime);
+    }
+};
+
+Arrow.prototype._createProjectile = function (x, y, angle) {
+    var p = new Projectile(this.game, x, y, "assets", "weapons/arrow", this, 
+        this._player, 100, angle, 300, 500, projectileOptions);
+    p.scale.setTo(1.8, 1.8);
+};
