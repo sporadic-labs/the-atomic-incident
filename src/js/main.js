dd3024108c0d1970e3538a23ba72a1b3af69ecf7
Michael Hadley <michaelwesthadley@gmail.com>
2016-11-13 14:59:31 -0600

    Switching to canvas for performance reasons (because of tilemap?)

--- src/js/main.js
+++ src/js/main.js
@@ -4,7 +4,7 @@ var LoadState = require("./states/load-state.js");
 var StartScreen = require("./states/start-screen.js");
 
 // Keep this on CANVAS until Phaser 3 for performance reasons?
-var game = new Phaser.Game(800, 600, Phaser.CANVAS, "game-container");
+var game = new Phaser.Game(800, 600, Phaser.AUTO, "game-container");
 
 game.state.add("boot", BootState);
 game.state.add("load", LoadState);
