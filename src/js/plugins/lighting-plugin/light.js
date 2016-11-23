c1cf383755ae6d4d52ebd2c4a830015fe01a93cb
Michael Hadley <michaelwesthadley@gmail.com>
2016-11-23 11:28:07 -0600

    Make lights flicker

--- src/js/plugins/lighting-plugin/light.js
+++ src/js/plugins/lighting-plugin/light.js
@@ -5,9 +5,14 @@ function Light(game, parent, position, radius, color) {
 	this.parent = parent;
 	this.position = position.clone();
 	this.radius = radius;
+    this.originalRadius = radius;
 	this.color = (color !== undefined) ? color : 0xFFFFFFFF;
     this._isDebug = false;
     this._debugGraphics = null;
+    this._noise = Simple1DNoise();
+    this._noise.setScale(0.15);
+    this._noise.setAmplitude(20);
+    this._frameCount = 0;
 }
 
 Light.prototype.enableDebug = function () {
@@ -26,6 +31,7 @@ Light.prototype.disableDebug = function () {
 };
 
 Light.prototype.update = function () {
+    this.radius = this.originalRadius + this._noise.getVal(this._frameCount++);
     if (this._debugGraphics) this._updateDebug();
 };
 
@@ -38,5 +44,56 @@ Light.prototype._updateDebug = function () {
     this._debugGraphics.position.copyFrom(this.position);
     this._debugGraphics.clear();
     this._debugGraphics.lineStyle(5, 0xFF00FF, 0.6);
+    this._debugGraphics.drawCircle(0, 0, 2);
     this._debugGraphics.drawCircle(0, 0, 2 * this.radius);
+};
+
+var Simple1DNoise = function() {
+    var MAX_VERTICES = 256;
+    var MAX_VERTICES_MASK = MAX_VERTICES -1;
+    var amplitude = 1;
+    var scale = 1;
+
+    var r = [];
+
+    for ( var i = 0; i < MAX_VERTICES; ++i ) {
+        r.push(Math.random());
+    }
+
+    var getVal = function( x ){
+        var scaledX = x * scale;
+        var xFloor = Math.floor(scaledX);
+        var t = scaledX - xFloor;
+        var tRemapSmoothstep = t * t * ( 3 - 2 * t );
+
+        /// Modulo using &
+        var xMin = xFloor & MAX_VERTICES_MASK;
+        var xMax = ( xMin + 1 ) & MAX_VERTICES_MASK;
+
+        var y = lerp( r[ xMin ], r[ xMax ], tRemapSmoothstep );
+
+        return y * amplitude;
+    };
+
+    /**
+    * Linear interpolation function.
+    * @param a The lower integer value
+    * @param b The upper integer value
+    * @param t The value between the two
+    * @returns {number}
+    */
+    var lerp = function(a, b, t ) {
+        return a * ( 1 - t ) + b * t;
+    };
+
+    // return the API
+    return {
+        getVal: getVal,
+        setAmplitude: function(newAmplitude) {
+            amplitude = newAmplitude;
+        },
+        setScale: function(newScale) {
+            scale = newScale;
+        }
+    };
 };
