b26e5b394ed61ee02eb5b1e0236cdc90ca4173a6
Rex Twedt <retwedt@gmail.com>
2016-12-18 15:06:37 -0600

    Initial commit of a Rocket launcher type weapon.  NOTE(rex): This is currently just a clone of the Grenade, but I actually think that weapon works kind of the way the rocket launcher should work.

---
+++ src/js/game-objects/weapons/rocket.js
@@ -0,0 +1,55 @@
+module.exports = Rocket;
+
+var BaseWeapon = require("./base-weapon.js");
+var BaseExplosive = require("./base-explosive.js");
+
+Rocket.prototype = Object.create(BaseWeapon.prototype);
+
+function Rocket(game, parentGroup, player) {
+    BaseWeapon.call(this, game, parentGroup, "Rocket", player);
+    this.initAmmo(30);
+    this.initCooldown(150);
+}
+
+Rocket.prototype.fire = function (targetPos) {
+    if (this.isAbleToAttack() && !this.isAmmoEmpty()) {
+        // Find trajectory
+        var angle = this._player.position.angle(targetPos); // Radians
+        // Start bullet in a position along that trajectory, but in front of 
+        // the player
+        var x = this._player.position.x + (0.75 * this._player.width) * 
+            Math.cos(angle);
+        var y = this._player.position.y + (0.75 * this._player.width) * 
+            Math.sin(angle);
+
+        this._createProjectile(x, y, angle);
+
+        this.incrementAmmo(-1);
+
+        this._startCooldown(this._cooldownTime);
+    }
+};
+
+Rocket.prototype.specialFire = function () {
+    if (this.isAbleToAttack() && this.getAmmo() > 0) {
+        // create 8 bullets evenly distributed in a circle
+        for (var i=0; i<=7; i++) {
+            // Start bullet in a position along that trajectory, but in front
+            // of the player
+            var angle = (i*(Math.PI/4));
+            var x = this._player.position.x + (0.75 * this._player.width) * 
+                Math.cos(angle);
+            var y = this._player.position.y + (0.75 * this._player.width) * 
+                Math.sin(angle);
+            this._createProjectile(x, y, angle);
+        }
+
+        this.incrementAmmo(-8);
+
+        this._startCooldown(this._specialCooldownTime);
+    }
+};
+
+Rocket.prototype._createProjectile = function (x, y, angle) {
+    new BaseExplosive(this.game, x, y, this, this._player, angle);
+};
