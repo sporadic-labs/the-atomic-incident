218f114c6d15f54763f6ddd81a6ee00a20e084da
Rex Twedt <retwedt@gmail.com>
2016-12-18 17:41:17 -0600

    Add a label with the type of ammo the pickup contains.

--- src/js/game-objects/pickups/weapon-pickup.js
+++ src/js/game-objects/pickups/weapon-pickup.js
@@ -12,39 +12,70 @@ WeaponPickup.prototype = Object.create(BasePickup.prototype);
 function WeaponPickup(game, x, y, type) {
     var pickups = game.globals.groups.pickups;
     BasePickup.call(this, game, x, y, "assets", "pickups/box-01", pickups,
-        "weapon", 0);
+        "weapon", 0, 10000);
 
+    // Figure out what type this pickup is supposed to be.
+    // Tint the box, and set a label.
     this.type = type;
-    this.ammoAmount = 0;
+    this._label;
     if (this.type === "weapon-sword") {
-        this.ammoAmount = 200;
         spriteUtils.applyRandomLightnessTint(this, 0.98, 1, 0.6);
+        this._label = game.make.image(0, 0, "assets", "weapons/sword");
+        this._label.scale.setTo(0.36, 0.36);
     } else if (this.type === "weapon-scattershot") {
-        this.ammoAmount = 200;
         spriteUtils.applyRandomLightnessTint(this, 0.67, 1, 0.6);
+        this._label = game.make.image(0, 0, "assets", "weapons/slug");
+        this._label.scale.setTo(0.25, 0.25);
     } else if (this.type === "weapon-flamethrower") {
-        this.ammoAmount = 200;
         spriteUtils.applyRandomLightnessTint(this, 0.16, 0.86, 0.6);
+        this._label = game.make.image(0, 0, "assets", "weapons/e-burst-01");
+        this._label.scale.setTo(0.42, 0.42);
     } else if (this.type === "weapon-machine-gun") {
-        this.ammoAmount = 200;
-        spriteUtils.applyRandomLightnessTint(this, 0.45, 1, 0.46);
+        spriteUtils.applyRandomLightnessTint(this, 1.0, 0.64, 0.46);
+        this._label = game.make.image(0, 0, "assets", "weapons/slug");
+        this._label.scale.setTo(0.42, 0.42);
     } else if (this.type === "weapon-laser") {
-        this.ammoAmount = 200;
         spriteUtils.applyRandomLightnessTint(this, 0.22, 0.7, 0.3);
+        this._label = game.make.image(0, 0, "assets", "weapons/laser-01");
+        this._label.scale.setTo(0.76, 0.76);
     } else if (this.type === "weapon-beam") {
-        this.ammoAmount = 200;
         spriteUtils.applyRandomLightnessTint(this, 0.54, 1, 0.6);
+        this._label = game.make.image(0, 0, "assets", "weapons/death-beam");
+        this._label.scale.setTo(0.56, 0.56);
     } else if (this.type === "weapon-arrow") {
-        this.ammoAmount = 200;
-        spriteUtils.applyRandomLightnessTint(this, 0.16, 0.42, 1.0);
-    } else if (this.type === "explosive") {
-        this.ammoAmount = 200;
-        spriteUtils.applyRandomLightnessTint(this, 0.16, 0.95, 1.0);
+        spriteUtils.applyRandomLightnessTint(this, 0.16, 0.42, 0.74);
+        this._label = game.make.image(0, 0, "assets", "weapons/arrow");
+        this._label.rotation =+ (Math.PI); // rotate 180 degrees
+    } else if (this.type === "grenade") {
+        spriteUtils.applyRandomLightnessTint(this, 0.71, 0.95, 0.21);
+        this._label = game.make.image(0, 0, "assets", "weapons/e-saw-02");
+        this._label.scale.setTo(0.9, 0.9);
+    } else if (this.type === "rocket") {
+        spriteUtils.applyRandomLightnessTint(this, 0.38, 1, 0.22);
+        this._label = game.make.image(0, 0, "assets", "weapons/slug");
+        this._label.scale.setTo(0.64, 0.96);
+    } else if (this.type === "weapon-slug") {
+        spriteUtils.applyRandomLightnessTint(this, 0.99, 0.15, 1.0);
+        this._label = game.make.image(0, 0, "assets", "weapons/slug");
+        this._label.scale.setTo(0.96, 0.96);
     }
 
-    // Setup animations
-    var idleFrames = Phaser.Animation.generateFrameNames("pickups/box-", 1, 4, 
-        "", 2);
-    this.animations.add(ANIM_NAMES.IDLE, idleFrames, 4, true);
-    this.animations.play(ANIM_NAMES.IDLE);
+    // Once the label has been chose, set the anchor and position,
+    // and add it to the foreground (so it is visible).
+    this._label.anchor.copyFrom(this.anchor);
+    this._label.position.copyFrom(this.position);
+    game.globals.groups.midground.add(this._label);
+
+    // NOTE(rex): This animation doesn't quite work with a label...
+    // // Setup animations
+    // var idleFrames = Phaser.Animation.generateFrameNames("pickups/box-", 1, 4, 
+    //     "", 2);
+    // this.animations.add(ANIM_NAMES.IDLE, idleFrames, 4, true);
+    // this.animations.play(ANIM_NAMES.IDLE);
 }
+
+WeaponPickup.prototype.destroy = function () {
+    // this._light.destroy();
+    this._label.destroy();
+    BasePickup.prototype.destroy.apply(this, arguments);
+};
