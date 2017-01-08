79a8ad08d2c9aad2eb10f595202b0b6a93cdacfc
Rex Twedt <retwedt@gmail.com>
2017-01-08 16:01:35 -0600

    Base explosive extends base projectile now.  NOTE(rex): This can probably change to be an explosion class that is created if a projectile has explosive properties and dies, instead of a projectile class in itself.

--- src/js/game-objects/weapons/base-explosive.js
+++ src/js/game-objects/weapons/base-explosive.js
@@ -1,8 +1,9 @@
 module.exports = BaseExplosive;
 
 var SpriteUtils = require("../../helpers/sprite-utilities.js");
+var BaseProjectile = require("./base-projectile.js");
 
-BaseExplosive.prototype = Object.create(Phaser.Sprite.prototype);
+BaseExplosive.prototype = Object.create(BaseProjectile.prototype);
 
 // Set Range & Life to -1 if you don't care about these options
 // options is an object containing some optional settings for the
@@ -12,39 +13,12 @@ BaseExplosive.prototype = Object.create(Phaser.Sprite.prototype);
 // - speedModifier - range (0 - 1.0)
 function BaseExplosive(game, x, y, key, frame, parentGroup, player, damage,
     angle, speed, range, life, options) {
-    Phaser.Sprite.call(this, game, x, y, key, frame);
-    this.anchor.set(0.5);
-    parentGroup.add(this);
+    // Phaser.Sprite.call(this, game, x, y, key, frame);
+    BaseProjectile.call(this, game, x, y, key, frame, parentGroup, player, 
+        damage, angle, speed, range, options);
 
     this._graphics = game.make.graphics(0, 0);
     this.addChild(this._graphics);
-
-    this._player = player;
-    this._enemies = game.globals.groups.enemies;
-    this._damage = damage;
-    this._speed = speed;
-    this._range = range;
-    this._life = life;
-    this._initialPos = this.position.clone();
-    this._remove = false; // check if BaseProjectile should be removed?
-
-    // projectile options
-    if (options !== undefined && options.isDestructible !== undefined)
-        this._isDestructable = options.isDestructible;
-    else this._isDestructable = true;
-    if (options !== undefined && options.rotateOnSetup !== undefined)
-        this._rotateOnSetup = options.rotateOnSetup;
-    else this._rotateOnSetup = true;
-    if (options !== undefined && options.speedModifier !== undefined)
-        this._speedModifier = options.speedModifier;
-    else this._speedModifier = 1.0;
-    // If rotateOnSetup option is true, rotate projectile to face in the
-    // right direction. Sprites are saved facing up (90 degrees), so we
-    // need to offset the angle
-    if (this._rotateOnSetup)
-        this.rotation = angle + (Math.PI / 2); // Radians
-    else
-        this.rotation = angle;
 
     // Store variable for explosion
     this._explosionRadius = 80;
@@ -53,24 +27,6 @@ function BaseExplosive(game, x, y, key, frame, parentGroup, player, damage,
     this._timer.start();
 
     this._hasExploded = false;
-
-    this.game.physics.arcade.enable(this);
-    this.game.physics.arcade.velocityFromAngle(angle * 180 / Math.PI, 
-        this._speed, this.body.velocity);
-
-    this.satBody = this.game.globals.plugins.satBody.addBoxBody(this);
-}
-
-BaseExplosive.prototype.update = function() {
-    // Collisions with the tilemap
-    SpriteUtils.satSpriteVsTilemap(this, this.game.globals.tileMapLayer, 
-        this._onCollideWithMap, this, 6);
-
-    // If there is a speed modifier, apply it.
-    if (this._speedModifier !== 1.0) {
-        this.body.velocity.x = this.body.velocity.x * this._speedModifier;
-        this.body.velocity.y = this.body.velocity.y * this._speedModifier;
-    }
 }
 
 BaseExplosive.prototype.explode = function () {
@@ -97,12 +53,12 @@ BaseExplosive.prototype.explode = function () {
 BaseExplosive.prototype.destroy = function () {
     this._graphics.destroy();
     this._timer.destroy();
-    Phaser.Sprite.prototype.destroy.apply(this, arguments);
+    BaseProjectile.prototype.destroy.apply(this, arguments);
 };
 
 BaseExplosive.prototype.postUpdate = function () {
     // Update arcade physics
-    Phaser.Sprite.prototype.postUpdate.apply(this, arguments);
+    BaseProjectile.prototype.postUpdate.apply(this, arguments);
     // Check overlap for the non-exploded projectile
     if (!this._hasExploded) {
         SpriteUtils.checkOverlapWithGroup(this, this._enemies, 
