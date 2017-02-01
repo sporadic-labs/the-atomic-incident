b7b820ea302ed9ef910ec990e8b70fb8e520caa7
Rex Twedt <retwedt@gmail.com>
2017-01-31 22:39:49 -0600

    Add some text to the HUD to indicate the currently selected tower.

--- src/js/game-objects/heads-up-display.js
+++ src/js/game-objects/heads-up-display.js
@@ -5,6 +5,7 @@ HeadsUpDisplay.prototype = Object.create(Phaser.Group.prototype);
 function HeadsUpDisplay(game, parentGroup) {
     Phaser.Group.call(this, game, parentGroup, "heads-up-display");
     
+    this.game = game;
     this._scoreKeeper = this.game.globals.scoreKeeper;
     this._player = this.game.globals.player;
     this._satBodyPlugin = this.game.globals.plugins.satBody;
@@ -21,6 +22,8 @@ function HeadsUpDisplay(game, parentGroup) {
     this.add(this._heartsText);
     this._coinsText = game.make.text(30, 60, "Coins: 0", textStyle);
     this.add(this._coinsText);
+    this._towerText = game.make.text(30, 100, "Tower: Pulse", textStyle);
+    this.add(this._towerText);
     this._debugText = game.make.text(30, game.height - 45, 
         "Debug ('E' key): false", textStyle);
     this._debugText.fontSize = 14;
@@ -34,6 +37,9 @@ function HeadsUpDisplay(game, parentGroup) {
 HeadsUpDisplay.prototype.update = function () {
     this._coinsText.setText("Coins: " + this._player.coins);
     this._heartsText.setText("Hearts: " + this._player.hearts);
+    var towerText = this.game.globals.towerList[this.game.globals.towerToPlace];
+    if (!towerText) towerText = "none";
+    this._towerText.setText("Tower: " + towerText);
 
     this._debugText.setText("Debug ('E' key): " + 
         this._satBodyPlugin.isDebugAllEnabled());
