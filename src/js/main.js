ed143af1b21237d93b995d5e7ecb6af2ad1f16d2
Michael Hadley <michaelwesthadley@gmail.com>
2017-04-19 09:30:19 -0500

    Babel polyfill

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,5 @@
+require("babel-polyfill");
+
 window.PIXI = require("phaser-ce/build/custom/pixi");
 window.p2 = require("phaser-ce/build/custom/p2");
 window.Phaser = require("phaser-ce/build/custom/phaser-split");
