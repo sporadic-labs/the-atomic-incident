4b95a87dcaebfce577d2dbfa3cac61e4769f0059
Michael Hadley <michaelwesthadley@gmail.com>
2017-04-20 17:40:45 -0500

    Delay tween component's start until after first update

--- src/js/game-objects/components/tween-path-component.js
+++ src/js/game-objects/components/tween-path-component.js
@@ -10,31 +10,40 @@ class TweenPathComponent {
      * @param {Path} path A path instance 
      * @param {number} speed Pixels per second 
      * @param {boolean} [shouldRepeat=true] Whether the tween should loop
-     * @param {boolean} [shouldPingPong=true] Whether the tween should ping pong
+     * @param {boolean} [shouldYoYo=true] Whether the tween should yoyo
      * 
      * @memberOf TweenPathComponent
      */
     constructor(owner, path, speed, shouldRepeat = true, 
-            shouldPingPong = true) {
+            shouldYoYo = true) {
         this.game = owner.game;
         this.owner = owner;
+        this._path = path;
 
         this._positionAlongPath = 0;
         const length = this._path.getLength();
         const duration = length / speed * 1000;
         
         this._tween = this.game.make.tween(this)
-            .to({_positionAlongPath: length}, duration, "Quad.easeInOut", 
-                true, 0, shouldRepeat, shouldPingPong);
+            .to({_positionAlongPath: length}, duration, "Quad.easeInOut")
+            .repeatAll(shouldRepeat ? -1 : 0)
+            .yoyo(shouldYoYo);
+        this._firstUpdate = true;
     }
 
     update() {
+        // Start the tween on the first update so that it doesn't run while 
+        // enemies are in their "spawning" state
+        if (this._firstUpdate) {
+            this._tween.start();
+            this._firstUpdate = false;
+        }
         const point = this._path.getPointAtLength(this._positionAlongPath);
         this.owner.position.copyFrom(point);
     }
 
     destroy() {
-        this._game.tweens.remove(this._tween);
+        this.game.tweens.remove(this._tween);
     }
 }
 
