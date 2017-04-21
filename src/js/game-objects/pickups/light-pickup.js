3a1b76390dd0c62ac3d4f443b387c5a4d7ecdcb1
Michael Hadley <michaelwesthadley@gmail.com>
2017-04-21 07:20:48 -0500

    LightPickup -> es6 class

--- src/js/game-objects/pickups/light-pickup.js
+++ src/js/game-objects/pickups/light-pickup.js
@@ -1,16 +1,15 @@
-module.exports = LightPickup;
-
 var Color = require("../../helpers/Color.js");
 
-LightPickup.prototype = Object.create(Phaser.Sprite.prototype);
-
-function LightPickup(game, x, y, parentGroup, color) {
-    Phaser.Sprite.call(this, game, x, y, "assets", "pickups/diamond-01");
-    this.color = (color instanceof Color) ? color : new Color(color);
-    this.tint = this.color.getRgbColorInt();
-    parentGroup.add(this);
-
-    // Configure physics
-    game.physics.arcade.enable(this);
-    this.satBody = game.globals.plugins.satBody.addBoxBody(this);
+class LightPickup extends Phaser.Sprite {
+    constructor(game, x, y, parentGroup, color) {
+        super(game, x, y, "assets", "pickups/diamond-01");
+        this.color = (color instanceof Color) ? color : new Color(color);
+        this.tint = this.color.getRgbColorInt();
+        parentGroup.add(this);
+        // Configure physics
+        game.physics.arcade.enable(this);
+        this.satBody = game.globals.plugins.satBody.addBoxBody(this);
+    }
 }
+
+module.exports = LightPickup;
