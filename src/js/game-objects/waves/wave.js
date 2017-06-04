b74f52b748dd4dbfc487fd9e6fadf26bac2ceaa1
Michael Hadley <michaelwesthadley@gmail.com>
2017-06-04 16:55:20 -0500

    New wave base class for sanity purposes

---
+++ src/js/game-objects/waves/wave.js
@@ -0,0 +1,31 @@
+class Wave {
+    /**
+     * Creates an instance of Wave.
+     * @param {Phaser.Game} game 
+     * 
+     * @memberof Wave
+     */
+    constructor(game) {
+        this.game = game;
+        this.composition = null;
+        this._levelManager = game.globals.levelManager;
+        this._enemies = game.globals.groups.enemies;
+    }
+
+    /** 
+     * @param {WaveComposition} composition The composition to use when spawning
+     * 
+     * @memberof Wave
+     */
+    // eslint-disable-next-line no-unused-vars
+    spawn(composition) {
+        // Subclasses should implement spawn!
+    }
+
+    destroy() {
+        // Subclass should implement destroy!
+    }
+
+}
+
+export default Wave;
