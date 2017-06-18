9e90801619ed79784eb26ea36b95a300e6d6f3e4
Michael Hadley <michaelwesthadley@gmail.com>
2017-06-17 22:50:03 -0500

    New structure for level data

---
+++ src/js/levels/generic-level.js
@@ -0,0 +1,65 @@
+export default class Level {
+    constructor(game, levelName) {
+        this.game = game;
+        this.name = levelName;
+        this.waves = [];
+        this.totalTime = 0;
+
+        this.waveStartedSignal = new Phaser.Signal();
+        
+        this._timer = game.time.create(false);
+    }
+
+    start() {
+        // Set up signal events for the start of each wave events
+        for (const [i, wave] of this.waves.entries()) {
+            this._timer.add(wave.startTime * 1000, () => {
+                this.waveStartedSignal.dispatch(i, wave);
+            });
+        }
+        this._timer.start();
+    }
+    
+    getSeconds() {
+        return this._timer.seconds;
+    }
+
+    /**
+     * @param {object} param Wave parameters
+     * @param {string} [param.mapName=null] Name of the map to load for the wave
+     * @param {number} [param.lightRadius=350] Size of the player's light for the wave
+     * @param {object[]} [param.ammoDrops=[]] Array describing when ammo pickups should drop
+     * @param {object[]} [param.enemyGroups=[]] Array describing when groups of enemies should spawn
+     * @memberof WaveData
+     */
+    addWave({mapName = null, lightRadius = 350, ammoDrops = [], enemyGroups = []}) {
+        // Start time of the wave
+        const startTime = this.totalTime;
+        // Total duration of the wave (e.g. time of the last enemy/drop to spawn)
+        let duration = 0;
+        for (const group of enemyGroups) duration = Math.max(duration, group.time);
+        for (const drop of ammoDrops) duration = Math.max(duration, drop.time);
+        // End time of the wave (factors in the delay)
+        const endTime = startTime + duration;
+
+        // Add the wave
+        this.waves.push({
+            startTime, endTime, enemyGroups, ammoDrops, mapName, lightRadius
+        });
+
+        // Update the level's total time
+        this.totalTime = endTime;
+
+        return this;
+    }
+
+    addWaveDelay(delay) {
+        this.totalTime += delay;
+        return this;
+    }
+
+    destroy() {
+        this._timer.destroy();
+    }
+}
+
---
+++ src/js/levels/puzzle-1.js
@@ -0,0 +1,180 @@
+import Composition from "../game-objects/waves/wave-composition";
+import PathTweenWave from "../game-objects/waves/path-tween-wave";
+import GenericLevel from "./generic-level";
+
+const mapName = "puzzle-map-1";
+const lightRadius = 400;
+
+/**
+ * This function returns a Level object
+ * @export
+ * @param {Phaser.Game} g 
+ * @returns {Level}
+ */
+export default function makeLevel(game) {
+    const level = new GenericLevel(game, "Puzzle 1");
+    addWaves(game, level);
+    return level;
+}
+
+/**
+ * @param {Phaser.Game} g 
+ * @param {Level} level 
+ */
+function addWaves(g, level) {
+    level
+    .addWaveDelay(2)
+    .addWave({
+        mapName,
+        lightRadius,
+        ammoDrops: [
+            {
+                time: 0, // Time relative to the start of the wave 
+                ammo: {red: 2, blue: 0, green: 0} // Amount of each color to spawn at this time
+            }  
+        ],
+        enemyGroups: [
+            {
+                time: 0, // Time relative to the start of the wave
+                composition: new Composition({red: 20, shield: null}),
+                wave: new PathTweenWave(g, {speed: 150, paths: ["paths-top-even:+1"]})
+            }, {
+                time: 3,
+                composition: new Composition({red: 20, shield: null}),
+                wave: new PathTweenWave(g, {speed: 150, paths: ["paths-bottom-even:-1"]})
+            }
+        ]
+    })
+    .addWaveDelay(10)
+    .addWave({
+        mapName,
+        lightRadius,
+        ammoDrops: [
+            {
+                time: 0,
+                ammo: {red: 0, blue: 1, green: 1}
+            }  
+        ],
+        enemyGroups: [
+            {
+                time: 2,
+                composition: new Composition({green: 20, shield: null}),
+                wave: new PathTweenWave(g, {speed: 150, paths: [
+                    "paths-top-even:+1", "paths-top-odd:+1"
+                ]})
+            }, {
+                time: 5,
+                composition: new Composition({blue: 20, shield: null}),
+                wave: new PathTweenWave(g, {speed: 150, paths: [
+                    "paths-bottom-even:+1", "paths-bottom-odd:+1"
+                ]})
+            }
+            
+        ]
+    })
+    .addWaveDelay(10)
+    .addWave({
+        mapName,
+        lightRadius,
+        ammoDrops: [
+            {
+                time: 0,
+                ammo: {red: 0, blue: 1, green: 0}
+            }  
+        ],
+        enemyGroups: [
+            {
+                time: 2,
+                composition: new Composition({blue: 20, shield: null}),
+                wave: new PathTweenWave(g, {speed: 150, paths: ["paths-top-even:+1"]})
+            }, {
+                time: 2,
+                composition: new Composition({blue: 20, shield: null}),
+                wave: new PathTweenWave(g, {speed: 150, paths: ["paths-bottom-even:-1"]})
+            }
+            
+        ]
+    })
+    .addWaveDelay(10)
+    .addWave({
+        mapName,
+        lightRadius,
+        ammoDrops: [
+            {
+                time: 0,
+                ammo: {red: 1, blue: 0, green: 1}
+            }  
+        ],
+        enemyGroups: [
+            {
+                time: 2,
+                composition: new Composition({red: 20, shield: null}),
+                wave: new PathTweenWave(g, {speed: 150, paths: [
+                    "paths-top-even:+1"
+                ]})
+            }, {
+                time: 2.25,
+                composition: new Composition({green: 20, shield: null}),
+                wave: new PathTweenWave(g, {speed: 150, paths: [
+                    "paths-bottom-even:+1"
+                ]})
+            }, {
+                time: 2.5,
+                composition: new Composition({red: 20, shield: null}),
+                wave: new PathTweenWave(g, {speed: 150, paths: [
+                    "paths-top-odd:+1"
+                ]})
+            }, {
+                time: 2.75,
+                composition: new Composition({green: 20, shield: null}),
+                wave: new PathTweenWave(g, {speed: 150, paths: [
+                    "paths-bottom-odd:+1"
+                ]})
+            }             
+        ]
+    })
+    .addWaveDelay(10)
+    .addWave({
+        mapName,
+        lightRadius,
+        ammoDrops: [
+            {
+                time: 0,
+                ammo: {red: 1, blue: 1, green: 1}
+            }
+        ],
+        enemyGroups: [
+            {
+                time: 0,
+                composition: new Composition({red: 20, shield: null}),
+                wave: new PathTweenWave(g, {speed: 150, paths: [
+                    "paths-top-even:+1"
+                ]})
+            }, {
+                time: 0,
+                composition: new Composition({red: 20, shield: null}),
+                wave: new PathTweenWave(g, {speed: 150, paths: [
+                    "paths-bottom-even:-1"
+                ]})
+            }, {
+                time: 1,
+                composition: new Composition({blue: 20, shield: null}),
+                wave: new PathTweenWave(g, {speed: 150, paths: [
+                    "paths-middle:+1"
+                ]})
+            }, {
+                time: 2,
+                composition: new Composition({green: 20, shield: null}),
+                wave: new PathTweenWave(g, {speed: 150, paths: [
+                    "paths-top-odd:+1"
+                ]})
+            }, {
+                time: 2,
+                composition: new Composition({green: 20, shield: null}),
+                wave: new PathTweenWave(g, {speed: 150, paths: [
+                    "paths-bottom-odd:+1"
+                ]})
+            }
+        ]
+    });
+}
