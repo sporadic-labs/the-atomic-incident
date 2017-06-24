cc1c183780510b6e6819747a6e3367167b75356e
rex <retwedt@gmail.com>
2017-06-24 18:02:11 -0500

    Trying to pull...

--- src/js/game-objects/waves/wave-menu.js
+++ src/js/game-objects/waves/wave-menu.js
@@ -1,3 +1,5 @@
+import ModalMenu from '../user-interface/modal-menu'
+
 const colors = require("../../constants/colors");
 
 let color = {
@@ -7,7 +9,7 @@ let color = {
     a: 1
 };
 
-class WaveMenu extends Phaser.Group {
+class WaveMenu extends ModalMenu {
 
     /**
      * Creates an instance of WaveMenu.
@@ -25,73 +27,70 @@ class WaveMenu extends Phaser.Group {
 
         // Shorthands
         var globals = game.globals;
-        var slickUI = globals.plugins.slickUI;
         var SlickUI = globals.plugins.SlickUI;
 
-        // Add a panel that will serve as the Menu container.
-        var panel = new SlickUI.Element.Panel((game.width * 0.5) - (game.width * 0.25), game.height * 0.12, game.width * 0.5, 
-            game.height / 4);
-        slickUI.add(panel);
         // Shorthand for panel dimensions
         const spacing = 4;
-        const pH = (panel.height / 10) + spacing;
-        const pW = panel.width / 2;
+        const pH = (this.panel.height / 10) + spacing;
+        const pW = this.panel.width / 2;
 
         // Add some things to the panel.
         // Title w/ Current Wave Number
-        const title = new SlickUI.Element.Text(0, pH, `Wave ${this._currentWaveIndex + 1}`);
-        panel.add(title).centerHorizontally().text.alpha = 0.5;
-        this._title = title;
+        this._title = this.addTitle(`Wave ${this._currentWaveIndex + 1}`, 0, 6);
 
         // Enemy composition
         const redEnemySprite = new SlickUI.Element.DisplayObject(pW - 36, pH * 2,
-            game.make.sprite(0, 0, "assets", "enemies/circle-idle"));
-        panel.add(redEnemySprite);
+            game.make.sprite(0, 0, "assets", "shadow-enemy/tintable-idle"));
+        this.panel.add(redEnemySprite);
         const redEnemies = new SlickUI.Element.Text(pW, pH * 2, `: 1`);
-        panel.add(redEnemies);
+        this.panel.add(redEnemies);
         this._redEnemies = redEnemies;
 
         const greenEnemySprite = new SlickUI.Element.DisplayObject(pW - 36, pH * 3,
-            game.make.sprite(0, 0, "assets", "enemies/circle-idle"));
-        panel.add(greenEnemySprite);
+            game.make.sprite(0, 0, "assets", "shadow-enemy/tintable-idle"));
+        this.panel.add(greenEnemySprite);
         const greenEnemies = new SlickUI.Element.Text(pW, pH * 3, `: 1`);
-        panel.add(greenEnemies);
+        this.panel.add(greenEnemies);
         this._greenEnemies = greenEnemies;
 
         const blueEnemySprite = new SlickUI.Element.DisplayObject(pW - 36, pH * 4,
-            game.make.sprite(0, 0, "assets", "enemies/circle-idle"));
-        panel.add(blueEnemySprite);
+            game.make.sprite(0, 0, "assets", "shadow-enemy/tintable-idle"));
+        this.panel.add(blueEnemySprite);
         const blueEnemies = new SlickUI.Element.Text(pW, pH * 4, `: 1`);
-        panel.add(blueEnemies);
+        this.panel.add(blueEnemies);
         this._blueEnemies = blueEnemies;
 
         // Ammo composition
         const redAmmoSprite = new SlickUI.Element.DisplayObject(pW - 36, pH * 5,
-            game.make.sprite(0, 0, "assets", "enemies/circle-idle"));
-        panel.add(redAmmoSprite);
+            game.make.sprite(0, 0, "assets", "shadow-enemy/tintable-idle"));
+        this.panel.add(redAmmoSprite);
         const redAmmo = new SlickUI.Element.Text(pW, pH * 5, `: 1`);
-        panel.add(redAmmo);
+        this.panel.add(redAmmo);
         this._redAmmo = redAmmo;
 
         const greenAmmoSprite = new SlickUI.Element.DisplayObject(pW - 36, pH * 6,
-            game.make.sprite(0, 0, "assets", "enemies/circle-idle"));
-        panel.add(greenAmmoSprite);
+            game.make.sprite(0, 0, "assets", "shadow-enemy/tintable-idle"));
+        this.panel.add(greenAmmoSprite);
         const greenAmmo = new SlickUI.Element.Text(pW, pH * 6, `: 1`);
-        panel.add(greenAmmo);
+        this.panel.add(greenAmmo);
         this._greenAmmo = greenAmmo;
 
         const blueAmmoSprite = new SlickUI.Element.DisplayObject(pW - 36, pH * 7,
-            game.make.sprite(0, 0, "assets", "enemies/circle-idle"));
-        panel.add(blueAmmoSprite);
+            game.make.sprite(0, 0, "assets", "shadow-enemy/tintable-idle"));
+        this.panel.add(blueAmmoSprite);
         const blueAmmo = new SlickUI.Element.Text(pW, pH * 7, `: 1`);
-        panel.add(blueAmmo);
+        this.panel.add(blueAmmo);
         this._blueAmmo = blueAmmo;
+
+        console.log(this.panel)
     }
 
     update() {}
 
     /**
-     * Setup the content of the Wave Menu.
+     * Update the content of the Wave Menu.
+     * 
+     * @method updateValues
      * 
      * @param {number} [waveNum=0] 
      * @param {number} [redEnemies=0] 
@@ -103,9 +102,10 @@ class WaveMenu extends Phaser.Group {
      * 
      * @memberof WaveMenu
      */
-    setupMenu(waveNum = 0, redEnemies = 0, greenEnemies = 0, blueEnemies = 0, redAmmo = 0, greenAmmo = 0, blueAmmo = 0) {
+    updateValues(waveNum = 0, redEnemies = 0, greenEnemies = 0, blueEnemies = 0, redAmmo = 0, greenAmmo = 0, blueAmmo = 0) {
         // Title
         this._title = `Wave ${waveNum}`;
+        console.log(this._title);
         // Enemy composition
         this._redEnemies = `: ${redEnemies}`;
         this._greenEnemies = `: ${greenEnemies}`;
@@ -115,10 +115,6 @@ class WaveMenu extends Phaser.Group {
         this._greenAmmo = `: ${greenAmmo}`;
         this._blueAmmo = `: ${blueAmmo}`;
     }
-
-    show() {}
-
-    hide() {}
 
     destroy() {
         super.destroy()
--- src/js/main.js
+++ src/js/main.js
@@ -1,17 +1,30 @@
 require("babel-polyfill");
+// DOM manipulation lib (NOT jQuery)
+require("chibijs");
 
 window.PIXI = require("phaser-ce/build/custom/pixi");
 window.p2 = require("phaser-ce/build/custom/p2");
 window.Phaser = require("phaser-ce/build/custom/phaser-split");
 
+const gameDimensions = 750;
+
 // Keep this on CANVAS until Phaser 3 for performance reasons?
 var game = new Phaser.Game({
-    width: 750, 
-    height: 750, 
+    width: gameDimensions, 
+    height: gameDimensions, 
     renderer: Phaser.WEBGL,
     enableDebug: true, // We can turn off debug when deploying - using debug causes a hit on webgl 
     parent: "game-container"
 });
+
+// Set HUD layer dimensions to match canvas.
+$("#hud").css("width", gameDimensions + "px");
+$("#hud").css("height", gameDimensions + "px");
+
+// Hide all of the menus to begin with.
+$("#start-menu").hide();
+$("options-menu").hide();
+$("pause-menu").hide();
 
 // Create the space for globals on the game object
 var globals = game.globals = {};
