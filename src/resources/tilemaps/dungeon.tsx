577a10f507d2d08cb559921f04ad6b2b3c786e8d
rex <retwedt@gmail.com>
2017-07-16 23:27:17 -0500

    Dungeon tileset and test tilemap.

---
+++ src/resources/tilemaps/dungeon.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="dungeon" tilewidth="25" tileheight="25" tilecount="48" columns="16">
+ <image source="dugeon_tileMap.png" width="400" height="75"/>
+</tileset>
