3573ed269ff6c3cb4ff1a33e9af3cd9df8c4b294
Michael Hadley <michaelwesthadley@gmail.com>
2017-09-04 09:12:17 -0500

    Es6 targeting component

--- src/js/game-objects/components/targeting-component.js
+++ src/js/game-objects/components/targeting-component.js
@@ -1,69 +1,68 @@
-// MH: if we end up using a component pattern often, we may want to create a
-// BaseComponent class that enforces that components have the standard lifecycle
-// methods of update/destroy/etc.
+// MH: if we end up using a component pattern often, we may want to create a BaseComponent class
+// that enforces that components have the standard lifecycle methods of update/destroy/etc.
 
 const SpriteUtils = require("../../helpers/sprite-utilities.js");
 
-module.exports = TargetingComponent;
-
-function TargetingComponent(parent, speed, visionRadius = null) {
-  this.game = parent.game;
-  this.parent = parent;
-  this.speed = speed;
-  this.target = this.game.globals.player;
-  this._visionRadius = visionRadius;
-  this._levelManager = this.game.globals.levelManager;
-}
-
-TargetingComponent.prototype.update = function() {
-  SpriteUtils.arcadeRecursiveCollide(this.parent, this.game.globals.groups.enemies);
-
-  // Stop moving
-  this.parent.body.velocity.set(0);
-
-  // Vision check, stop early if target is out of range
-  if (this._visionRadius !== null) {
-    const d = this.parent.position.distance(this.target.position);
-    if (d > this._visionRadius) return this._target;
+export default class TargetingComponent {
+  constructor(parent, speed, visionRadius = null) {
+    this.game = parent.game;
+    this.parent = parent;
+    this.speed = speed;
+    this.target = this.game.globals.player;
+    this._visionRadius = visionRadius;
+    this._levelManager = this.game.globals.levelManager;
   }
 
-  // Calculate path
-  const path = this._levelManager
-    .getCurrentNavMesh()
-    .findPath(this.parent.position, this.target.position);
+  update() {
+    SpriteUtils.arcadeRecursiveCollide(this.parent, this.game.globals.groups.enemies);
 
-  // Check if there is a path that was found
-  if (path) {
-    if (path.length > 1) {
-      // If there are multiple steps in the path, head towards the second
-      // point. This allows the sprite to skip the tile it is currently in.
-      var nextNode = path[1];
-      var nextTargetPoint = new Phaser.Point(nextNode.x, nextNode.y);
-      this._moveTowards(nextTargetPoint);
-    } else {
-      // If there aren't multiple steps, sprite is close enough to directly head
-      // for the target itself
-      this._moveTowards(this.target.position);
+    // Stop moving
+    this.parent.body.velocity.set(0);
+
+    // Vision check, stop early if target is out of range
+    if (this._visionRadius !== null) {
+      const d = this.parent.position.distance(this.target.position);
+      if (d > this._visionRadius) return this._target;
     }
+
+    // Calculate path
+    const path = this._levelManager
+      .getCurrentNavMesh()
+      .findPath(this.parent.position, this.target.position);
+
+    // Check if there is a path that was found
+    if (path) {
+      if (path.length > 1) {
+        // If there are multiple steps in the path, head towards the second
+        // point. This allows the sprite to skip the tile it is currently in.
+        var nextNode = path[1];
+        var nextTargetPoint = new Phaser.Point(nextNode.x, nextNode.y);
+        this._moveTowards(nextTargetPoint);
+      } else {
+        // If there aren't multiple steps, sprite is close enough to directly head
+        // for the target itself
+        this._moveTowards(this.target.position);
+      }
+    }
+
+    return this.target;
   }
 
-  return this.target;
-};
+  _moveTowards(position) {
+    var angle = this.parent.position.angle(position);
 
-TargetingComponent.prototype._moveTowards = function(position) {
-  var angle = this.parent.position.angle(position);
+    // Move towards target
+    var distance = this.parent.position.distance(position);
+    var targetSpeed = distance / this.game.time.physicsElapsed;
+    var magnitude = Math.min(this.speed, targetSpeed);
+    this.parent.body.velocity.x = magnitude * Math.cos(angle);
+    this.parent.body.velocity.y = magnitude * Math.sin(angle);
 
-  // Move towards target
-  var distance = this.parent.position.distance(position);
-  var targetSpeed = distance / this.game.time.physicsElapsed;
-  var magnitude = Math.min(this.speed, targetSpeed);
-  this.parent.body.velocity.x = magnitude * Math.cos(angle);
-  this.parent.body.velocity.y = magnitude * Math.sin(angle);
+    // Rotate towards target
+    this.parent.rotation = angle + Math.PI / 2;
+  }
 
-  // Rotate towards target
-  this.parent.rotation = angle + Math.PI / 2;
-};
-
-TargetingComponent.prototype.destroy = function() {
-  // Nothing special to destroy
-};
+  destroy() {
+    // Nothing special to destroy
+  }
+}
