46a34143c92e6a90be417a0cbacb6bc93ddb56b6
rex <retwedt@gmail.com>
2017-09-23 14:56:41 -0500

    #63 Initial commit of Game Over menu, shown when player is KIA.

---
+++ src/js/menu/components/game-over-menu.js
@@ -0,0 +1,15 @@
+import { h } from "preact";
+
+export default function GameOverMenu({ gameStore, onMainMenu, onRestart }) {
+  return (
+    <div id="game-over" class="menu">
+      <button class="btn-close" onClick={onMainMenu}>
+        <i class="fa fa-times" aria-hidden="true" />
+      </button>
+      <div class="menu-title">Game Over</div>
+      <div class="final-score">Your Score: {gameStore.score}</div>
+      <button onClick={onRestart}>Restart</button>
+      <button onClick={onMainMenu}>Main Menu</button>
+    </div>
+  );
+}
