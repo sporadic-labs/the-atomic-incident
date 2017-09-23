9e9cd249d3c6729c7c6fd8b002792c58b1f9b170
rex <retwedt@gmail.com>
2017-09-23 16:58:23 -0500

    #63 Show the highscore in the Game Over menu.  NOTE(rex): This isn't actually updated.

--- src/js/menu/components/game-over-menu.js
+++ src/js/menu/components/game-over-menu.js
@@ -8,6 +8,7 @@ export default function GameOverMenu({ gameStore, onMainMenu, onRestart }) {
       </button>
       <div class="menu-title">Game Over</div>
       <div class="final-score">Your Score: {gameStore.score}</div>
+      <div class="final-score">Current High Score: {gameStore.highScore}</div>
       <button onClick={onRestart}>Restart</button>
       <button onClick={onMainMenu}>Main Menu</button>
     </div>
