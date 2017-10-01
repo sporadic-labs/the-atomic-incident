c77d17c20d6fb693848f5e6b4e8efe8560a379e2
rex <retwedt@gmail.com>
2017-10-01 15:08:02 -0500

    Show high score on the home and pause menu.

--- src/js/menu/components/start-menu.js
+++ src/js/menu/components/start-menu.js
@@ -1,9 +1,10 @@
 import { h } from "preact";
 
-export default function StartMenu({ onStart, onOptions }) {
+export default function StartMenu({ gameStore, onStart, onOptions }) {
   return (
     <div id="pause-menu" class="menu">
       <div class="menu-title">Untitled Game!</div>
+      <div class="final-score">High Score: {gameStore.highScore}</div>
       <button onClick={onStart}>Start Game</button>
       <button onClick={onOptions}>Options</button>
     </div>
