da89b7186226c425ab0f2812ccac13e9c6ea5197
Michael Hadley <michaelwesthadley@gmail.com>
2018-02-05 20:16:58 -0600

    Copy over SAT factory helpers

---
+++ src/js/plugins/sat-body-plugin-revisited/sat-factories.js
@@ -0,0 +1,21 @@
+import SAT from "sat";
+
+export function vec(x, y) {
+  return new SAT.Vector(x, y);
+}
+
+export function pointToVec(p) {
+  return new SAT.Vector(p.x, p.y);
+}
+
+export function box(pos, w, h) {
+  return new SAT.Box(pos, w, h);
+}
+
+export function circle(pos, r) {
+  return new SAT.Circle(pos, r);
+}
+
+export function polygon(pos, points) {
+  return new SAT.Polygon(pos, points);
+}
