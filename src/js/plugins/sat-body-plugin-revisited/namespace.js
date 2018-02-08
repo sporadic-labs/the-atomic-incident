3bb082148b04f464dc52a863174bb6efd79a4137
Michael Hadley <michaelwesthadley@gmail.com>
2018-02-08 08:30:58 -0600

    Plugin installs itself to Phaser.Physics.SAT and Phaser.Physics.Sat

---
+++ src/js/plugins/sat-body-plugin-revisited/namespace.js
@@ -0,0 +1,15 @@
+import World from "./world";
+import Factory from "./factory";
+import Body from "./body";
+import Collider from "./collider";
+import BODY_TYPES from "./body-types";
+import BODY_SHAPES from "./body-shapes";
+
+export default {
+  World,
+  Factory,
+  Body,
+  Collider,
+  BODY_TYPES,
+  BODY_SHAPES
+};
