34e85cdd2d9182813b2e264a89d52015dbcbf951
Michael Hadley <michaelwesthadley@gmail.com>
2018-02-08 08:25:52 -0600

    Testing tilemap collision against hull map vs individual tiles

--- src/js/states/sat-body-test.js
+++ src/js/states/sat-body-test.js
@@ -13,6 +13,7 @@ import { MENU_STATE_NAMES } from "../menu";
 import { gameStore, preferencesStore } from "../game-data/observable-stores";
 import { autorun } from "mobx";
 import MapManager from "../game-objects/level-manager";
+import Player from "../game-objects/player";
 
 export default class LightingPerf extends Phaser.State {
   create() {
@@ -69,102 +70,88 @@ export default class LightingPerf extends Phaser.State {
     const sat = this.physics.sat;
     sat.world.enableDebug(this.game.add.graphics(0, 0, globals.groups.hud));
 
-    const sprite1 = game.add.sprite(200, 250, "assets", "physics-test/box", globals.groups.game);
-    const body1 = this.physics.sat.add
-      .gameObject(sprite1, { shape: "rectangle" })
-      .setVelocity(100, 25);
-    this.body1 = body1;
-    this.physics.sat.add.collider(sprite1, this.game.globals.mapManager.wallLayer);
+    // const sprite1 = game.add.sprite(200, 250, "assets", "physics-test/box", globals.groups.game);
+    // const body1 = this.physics.sat.add
+    //   .gameObject(sprite1, { shape: "rectangle" })
+    //   .setPosition(300, 250);
+    // this.body1 = body1;
+    // this.physics.sat.add.collider(sprite1, this.game.globals.mapManager.wallLayer);
 
-    const sprite2 = game.add.sprite(250, 200, "assets", "physics-test/box", globals.groups.game);
-    const body2 = sat.add.body({ gameObject: sprite2, shape: { type: "circle", radius: 40 } });
-    this.body2 = body2;
-    this.physics.sat.add.collider(sprite2, this.game.globals.mapManager.wallLayer);
+    // const sprite2 = game.add.sprite(250, 200, "assets", "physics-test/box", globals.groups.game);
+    // const body2 = sat.add.body({ gameObject: sprite2, shape: { type: "circle", radius: 40 } });
+    // this.body2 = body2;
+    // this.physics.sat.add.collider(sprite2, this.game.globals.mapManager.wallLayer);
 
-    const circleBody = sat.add
-      .body({ shape: { type: "circle", radius: 25 } })
-      .setPosition(250, 300)
-      .setVelocity(-200, 150);
-    this.circleBody = circleBody;
-    this.physics.sat.add.collider(circleBody, this.game.globals.mapManager.wallLayer);
+    // const circleBody = sat.add
+    //   .body({ shape: { type: "circle", radius: 25 } })
+    //   .setPosition(250, 300)
+    //   .setVelocity(-200, 150);
+    // this.circleBody = circleBody;
+    // this.physics.sat.add.collider(circleBody, this.game.globals.mapManager.wallLayer);
 
-    const group = this.game.add.group();
-    for (let i = 0; i < 8; i++) {
-      const x = this.game.rnd.integerInRange(200, 400);
-      const y = this.game.rnd.integerInRange(200, 400);
-      const body = sat.add
+    // const group = this.game.add.group();
+    // for (let i = 0; i < 8; i++) {
+    //   const x = this.game.rnd.integerInRange(200, 400);
+    //   const y = this.game.rnd.integerInRange(200, 400);
+    //   const body = sat.add
+    //     .body({
+    //       gameObject: game.add.sprite(x, y, "assets", "physics-test/box", globals.groups.game)
+    //     })
+    //     .setRectangle(50, 50)
+    //     .setVelocity(
+    //       this.game.rnd.integerInRange(-400, 400),
+    //       this.game.rnd.integerInRange(-400, 400)
+    //     );
+    //   group.add(body.gameObject);
+    // }
+    // this.group = group;
+    // this.physics.sat.add.collider(group, this.game.globals.mapManager.wallLayer);
+
+    // const key = "enemies/particle-tank/particle-tank";
+    // const polySprite = game.add.sprite(500, 300, "assets", key, globals.groups.game);
+    // const points = [[10, 31], [37, 2], [64, 29], [72, 55], [11, 58], [3, 51.2]].map(p => ({
+    //   x: p[0] - 75 / 2,
+    //   y: p[1] - 75 / 2
+    // }));
+    // const polyBody = this.physics.sat.add
+    //   .gameObject(polySprite)
+    //   .setPolygon(points)
+    //   .setVelocity(-100, 25);
+    // polyBody.gameObject.anchor.set(0.5);
+
+    // this.physics.sat.add.collider(group, polySprite);
+    // this.physics.sat.add.collider(polySprite, this.game.globals.mapManager.wallLayer);
+
+    // Player
+    // Setup a new player, and attach it to the global variabls object.
+    const spawnObjects = mapManager.tilemap.objects["player-spawn"] || [];
+    const spawnPoint =
+      spawnObjects.length > 0
+        ? { x: spawnObjects[0].x, y: spawnObjects[0].y }
+        : { x: this.world.width / 2, y: this.world.height / 2 };
+    const player = new Player(game, spawnPoint.x, spawnPoint.y, groups.foreground);
+    globals.player = player;
+    this.player = player;
+
+    const tileCollisionMap = mapManager.tilemap.objects["collision-map"] || [];
+    this._tileRects = tileCollisionMap.map(r =>
+      this.physics.sat.add
         .body({
-          gameObject: game.add.sprite(x, y, "assets", "physics-test/box", globals.groups.game)
+          shape: { type: "rectangle", width: r.width, height: r.height },
+          bodyType: "static"
         })
-        .setRectangle(50, 50)
-        .setVelocity(
-          this.game.rnd.integerInRange(-400, 400),
-          this.game.rnd.integerInRange(-400, 400)
-        );
-      group.add(body.gameObject);
-    }
-    this.group = group;
-    this.physics.sat.add.collider(group, this.game.globals.mapManager.wallLayer);
+        .setPosition(r.x, r.y)
+    );
 
-    const key = "enemies/particle-tank/particle-tank";
-    const polySprite = game.add.sprite(500, 300, "assets", key, globals.groups.game);
-    const points = [[10, 31], [37, 2], [64, 29], [72, 55], [11, 58], [3, 51.2]].map(p => ({
-      x: p[0] - 75 / 2,
-      y: p[1] - 75 / 2
-    }));
-    const polyBody = this.physics.sat.add
-      .gameObject(polySprite)
-      .setPolygon(points)
-      .setVelocity(-100, 25);
-    polyBody.gameObject.anchor.set(0.5);
-
-    this.physics.sat.add.collider(group, polySprite);
-    this.physics.sat.add.collider(polySprite, this.game.globals.mapManager.wallLayer);
-
-    // Extend right check:
-    // sprite1.position.setTo(200, 250);
-    // body1
-    //   .setPosition(200, 250)
-    //   .setVelocity(200, 150)
-    //   .setBounce(1);
-
-    // Extend down check:
-    // sprite1.position.setTo(550, 300);
-    // body1
-    //   .setPosition(550, 300)
-    //   .setVelocity(-250, -100)
-    //   .setBounce(1);
-
-    // this.input.onDown.add(() => {
-    //   const x = this.input.x;
-    //   const y = this.input.y;
-    //   body1
-    //     .setPosition(x, y)
-    //     .setVelocity(200, 100)
-    //     .setBounce(1);
-    //   console.log(x, y);
-    // });
-
-    // this.input.onDown.add(() => {
-    //   const b = new Body(satWorld, {
-    //     gameObject: game.add.sprite(
-    //       this.input.x,
-    //       this.input.y,
-    //       "assets",
-    //       "physics-test/box",
-    //       globals.groups.game
-    //     )
-    //   }).setRectangle(50, 50);
-    //   satWorld.add(b);
-    //   b.gameObject.rotation = this.game.rnd.between(0, 2 * Math.PI);
-    //   b.bounce = 1;
-    //   b.velocity.setTo(this.game.rnd.between(-200, 200), this.game.rnd.between(-200, 200));
-    // });
+    game.world.setBounds(0, 0, mapManager.tilemap.widthInPixels, mapManager.tilemap.heightInPixels);
+    game.camera.follow(player);
   }
 
   update() {
     // this.b1.gameObject.rotation += 0.005;
     // this.body2.position.copyFrom(this.input.position);
+
+    this._tileRects.forEach(tb => this.physics.sat.world.collide(this.player, tb));
 
     if (this._fpsText) {
       this._fpsText.setText(this.game.time.fps);
