22b7a8fe541712af5f06e43131966da5159eca4d
rex <retwedt@gmail.com>
2018-02-10 15:07:34 -0600

    Not using this code, and prob won't have a reload animation.

--- src/js/game-objects/hud/ammo.js
+++ src/js/game-objects/hud/ammo.js
@@ -46,43 +46,4 @@ export default class Ammo extends Phaser.Group {
     const w = this._player.weaponManager.getActiveWeapon();
     this._nameText.setText(`(insert image)\n${w.getName()}`);
   }
-
-  // /**
-  //  * Start the hud reload animation.
-  //  * TODO(rex): Do something better here...
-  //  *
-  //  * @param {int} duration
-  //  * @memberof HeadsUpDisplay
-  //  */
-  // startHudReloadAnimation(duration) {
-  //   // Set variables used for the HUD reload animation.
-  //   this._reloadAnimationDuration = duration; // TODO(rex): Default.
-  //   this._reloadAnimationStartTime = Date.now();
-  //   this._playReloadAnimation = true;
-  // }
-
-  // /**
-  //  * Manually updating the mask around the reload text.
-  //  * TODO(rex): This isn't being used right now...
-  //  *
-  //  * @memberof HeadsUpDisplay
-  //  */
-  // _hudReloadAnimation() {
-  //   // Get the current time, figure our progress through the animation timer,
-  //   // and update the hud accordingly.
-  //   const currentTime = Date.now();
-  //   const diff = currentTime - this._reloadAnimationStartTime;
-  //   if (diff < this._reloadAnimationDuration) {
-  //     // Update the reload mask.
-  //     const fractionMasked = diff / this._reloadAnimationDuration;
-  //     const cropAmount = fractionMasked * this._ammoText.width;
-  //     this._ammoText.crop(new Phaser.Rectangle(0, 0, cropAmount, this._ammoText.height));
-  //   } else {
-  //     // The animation has ended, reset animation variables.
-  //     this._playReloadAnimation = false;
-  //     this._reloadAnimationStartTime = 0;
-  //     this._reloadAnimationDuration = 0; // TODO(rex): This...?
-  //     this._ammoMask.visible = false;
-  //   }
-  // }
 }
