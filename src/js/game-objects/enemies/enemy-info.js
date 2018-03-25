fb9171a98a4d3296f07bf4d14473a9b0150a31f5
rex <retwedt@gmail.com>
2018-03-25 15:46:30 -0500

    Update enemies with new sprites.

--- src/js/game-objects/enemies/enemy-info.js
+++ src/js/game-objects/enemies/enemy-info.js
@@ -19,42 +19,42 @@ export const ENEMY_INFO = {
     animated: false,
     health: 100,
     speed: 160,
-    key: "enemies_v2/splitting_enemy_large_50_0",
+    key: "test/enemy_splitting_large/test_0",
     collisionPoints: physicsShapes["amoeba"]
   },
   DIVIDING_SMALL: {
     animated: false,
     health: 100,
     speed: 160,
-    key: "enemies_v2/splitting_enemy_small_50_0",
+    key: "test/enemy_splitting_small/test_0",
     collisionPoints: physicsShapes["amoeba"]
   },
   FOLLOWING: {
     animated: false,
     health: 100,
     speed: 160,
-    key: "enemies_v2/tracking_enemy_small_50_0",
+    key: "test/enemy_tracking_small/test_0",
     collisionPoints: physicsShapes["beetle"]
   },
   TANK: {
     animated: false,
     health: 150,
     speed: 100,
-    key: "enemies_v2/charging_mini_boss_50_0",
+    key: "test/mini_boss_charging/test_0",
     collisionPoints: physicsShapes["particle-tank"]
   },
   DASHING: {
     animated: false,
     health: 100,
     speed: 160,
-    key: "enemies_v2/charging_enemy_50_0",
+    key: "test/enemy_charging/test_0",
     collisionPoints: physicsShapes["worm"]
   },
   PROJECTILE: {
     animated: false,
     health: 100,
     speed: 160,
-    key: "enemies_v2/shooting_enemy_50_0",
+    key: "test/enemy_shooting/test_0",
     collisionPoints: physicsShapes["virus-dark"]
   }
 };
