1e196c6ef5ba784fe441dba9d20271cca88f83bc
Michael Hadley <michaelwesthadley@gmail.com>
2018-04-01 16:42:25 -0500

    Remove FA from fontobserver config

--- src/js/fonts/fonts.js
+++ src/js/fonts/fonts.js
@@ -1,8 +1,7 @@
 const fonts = [
   { family: "Montserrat", weights: [300, 400], styles: ["normal"] },
   { family: "Alfa Slab One", weights: [400], styles: ["normal"] },
-  { family: "Cherry Cream Soda", weights: [400], styles: ["normal"] },
-  { family: "FontAwesome", weights: [400], styles: ["normal"] }
+  { family: "Cherry Cream Soda", weights: [400], styles: ["normal"] }
 ];
 
 export default fonts;
