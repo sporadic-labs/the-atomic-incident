a5a543f7978183758fb4678722a92b2ff88e3021
Michael Hadley <michaelwesthadley@gmail.com>
2018-04-01 21:53:55 -0500

    Remove Cherry Cream Soda font

--- src/js/fonts/fonts.js
+++ src/js/fonts/fonts.js
@@ -1,7 +1,6 @@
 const fonts = [
   { family: "Montserrat", weights: [300, 400], styles: ["normal"] },
   { family: "Alfa Slab One", weights: [400], styles: ["normal"] },
-  { family: "Cherry Cream Soda", weights: [400], styles: ["normal"] }
 ];
 
 export default fonts;
